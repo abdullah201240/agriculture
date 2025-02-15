4492d5d54ebabf52829a6ca5fbf20b54fca4b82c
abdullah201240 <abdullahalsakib7075@gmail.com>
2025-02-15 19:51:52 +0600

    1

--- client/src/app/user/dashboard/page.tsx
+++ client/src/app/user/dashboard/page.tsx
@@ -1,8 +1,23 @@
+'use client';
 import Footer from '@/components/Footer'
 import UserNavbar from '@/components/UserNavbar'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/navigation';
+export default function Page() {
+  const router = useRouter();
 
-export default function page() {
+  useEffect(() => {
+    const checkSession = async () => {
+      const storedUserInfo = localStorage.getItem('sessionToken');
+      if (!storedUserInfo) {
+        router.push('login');
+        return;
+      }
+    };
+
+    checkSession();
+  }, [router]);
+
   return (
     <div>
         <UserNavbar/>
